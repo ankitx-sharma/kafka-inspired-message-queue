29e59c88d38faa426a6e8aea61d6c416e252f5b3
Ankita Sharma <ankitasharma.281993@gmail.com>
2025-12-25 17:11:21 +0100

    configured the controller for sending out event data

--- messaging-api/src/main/java/org/main/api/controller/StreamController.java
+++ messaging-api/src/main/java/org/main/api/controller/StreamController.java
@@ -1,5 +1,22 @@
 package org.main.api.controller;
 
-public class StreamController {
+import org.main.api.service.SseHub;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+@RestController
+@RequestMapping("/api/events")
+public class StreamController {
+	private final SseHub ssehub;
+	
+	public StreamController(SseHub ssehub) {
+		this.ssehub = ssehub;
+	}
+	
+	@GetMapping("/stream")
+	public SseEmitter stream() {
+		return ssehub.subscribe();
+	}
 }

8485e57d2efff29c24abca5163a341ac03c1a8d6
Ankita Sharma <ankitasharma.281993@gmail.com>
2025-12-26 17:28:47 +0100

    updates to the class based on new changes in thread pool class

--- messaging-engine/src/main/java/org/main/engine/MainExecutorClass.java
+++ messaging-engine/src/main/java/org/main/engine/MainExecutorClass.java
@@ -3,19 +3,17 @@ package org.main.engine;
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.main.engine.listener.EngineEventPublisher;
 import org.main.engine.processor.WorkerThreadPoolProcessor;
 
 public class MainExecutorClass {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
-		WorkerThreadPoolProcessor threadPool = new WorkerThreadPoolProcessor(4, 4);
+		WorkerThreadPoolProcessor threadPool = new WorkerThreadPoolProcessor(4, 4, new EngineEventPublisher());
 		
 		for(int i=0; i<20; i++) {
 			threadPool.submitTask(Task.generateTask());
 		}
-		
-		
-		
 		threadPool.shutdownGracefully();
 	}
 }

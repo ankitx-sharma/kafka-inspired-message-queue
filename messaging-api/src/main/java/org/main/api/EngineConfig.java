a9b9ccc2401ca7eb3ad5b91dddf1768796024247
Ankita Sharma <ankitasharma.281993@gmail.com>
2025-12-26 22:16:48 +0100

    removed bean configuration as no longer required

--- messaging-api/src/main/java/org/main/api/EngineConfig.java
+++ messaging-api/src/main/java/org/main/api/EngineConfig.java
@@ -6,10 +6,10 @@ import org.main.engine.processor.MessagingEngine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class EngineConfig {
 
-	@Bean
+	//@Bean
 	public MessagingEngine messagingEngine() throws IOException {
 		return new MessagingEngine(4, 100, 10_000);
 	}

f2c30d9cd3de3a5966e4b9c3d603d80c6cf95317
Ankita Sharma <ankitasharma.281993@gmail.com>
2025-12-28 15:54:29 +0100

    changes in ui to correctly update the engine status

--- messaging-ui/src/api/useRunState.js
+++ messaging-ui/src/api/useRunState.js
@@ -15,7 +15,7 @@ export function useRunStatus(){
         }
 
         poll();
-        const t = setInterval(poll, 5000);
+        const t = setInterval(poll, 100);
         return () => {
             alive = false;
             clearInterval(t);
